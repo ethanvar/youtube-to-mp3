24c9a1e690f0c390753b492aa201dbc7108b7478
ethanvar <ethanvar6@gmail.com>
2024-05-26 20:46:10 -0400

    Created audio wave with wavesurfer.js

--- splitter-client/src/app/page.tsx
+++ splitter-client/src/app/page.tsx
@@ -11,18 +11,12 @@ function Searchbar(){
 }
 
 export default async function Page() {
-  const res2 = await fetch('https://ac16-174-91-124-30.ngrok-free.app/audio/Reality-Surf.mp3')
-  const res = await fetch('https://ac16-174-91-124-30.ngrok-free.app/tracks', {cache: 'no-store'})
+  const res2 = await fetch('https://7d8f-174-91-124-30.ngrok-free.app/audio/Reality-Surf.mp3')
+
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div>
-        <Searchbar />   
-      </div>
-      <div>
-        <Trackdisplay title="Track One" song={res2.url} />
-      </div>
-      <div>
-        <Trackdisplay title="Track Two" song={res2.url}/>
+    <div>
+      <div className='flex min-h-screen items-center justify-center'>
+          <Trackdisplay title="Track One" song={res2.url} /> 
       </div>
     </div>
   );
--- splitter-client/src/components/trackdisplay.tsx
+++ splitter-client/src/components/trackdisplay.tsx
@@ -1,103 +1,46 @@
 "use client"
 import React from 'react';
-import { useRef, useEffect, useState } from 'react';
-import WaveSurfer from "wavesurfer.js";
-import Regions from 'wavesurfer.js/dist/plugins/regions.esm.js'
+import { useRef } from 'react';
+import { useWavesurfer } from '@wavesurfer/react'
 
 interface Props {
-  /** The text to display inside the button */
-  title: string;
-  /** Whether the button can be interacted with */
-  song: string;
+  "title" : string,
+  "song" : string
 }
 
-export const Trackdisplay = ( {title, song}: Props) => {
-
-    const waveformRef = React.useRef<HTMLDivElement | null>(null);
-    const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
-    const [isPlaying, setIsPlaying] = useState<boolean>(false);
-    useEffect(() => {
-      if (waveformRef.current) {
-        console.log("HELLO")
-        const ws = WaveSurfer.create({
-          container: waveformRef.current,
-          waveColor: "#34374B",
-          progressColor: "#F90",
-          url: song,
-          dragToSeek: true,
-          height: 60,
-          barHeight: 20,
-          barRadius: 20,
-          barWidth: 5,
-        });
+export const Trackdisplay = ( {title, song} : Props) => {
+  const waveformRef = useRef(null);
+  console.log(song)
+  const { wavesurfer, isReady, isPlaying, currentTime } = useWavesurfer({
+    container: waveformRef,
+    url: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Ulaw_Lalapa_sakatusa.ogg',
+    waveColor: "turquoise",
+    height: 100,
+    width: 1000,
+    hideScrollbar: true,
+    backend: 'MediaElement',
+    normalize: true,
+    autoplay: false,
+    barGap: 1,
+    barHeight: 20,
+    barRadius: 20,
+    barWidth: 5,
+  })
   
-        ws.on("finish", () => {
-          console.log("Song finished");
-          setIsPlaying(false);
-        });
-  
-        ws.on("ready", () => {
-          console.log("Waveform is ready");
-        });
-  
-        setWavesurfer(ws);
-  
-        return () => {
-          ws.destroy();
-        };
-      }
-    }, []);
-
-    const handleStop = () => {
-      if (wavesurfer) {
-        wavesurfer.stop();
-        setIsPlaying(false);
-      }
-    };
-  
-    const handlePlayPause = () => {
-      if (wavesurfer) {
-        wavesurfer.playPause();
-        setIsPlaying(!isPlaying);
-      }
-    };
-  
-    const handleSkipForward = () => {
-      if (wavesurfer) {
-        wavesurfer.skip(2);
-      }
-    };
-  
-    const handleSkipBack = () => {
-      if (wavesurfer) {
-        wavesurfer.skip(-2);
-      }
-    };
-  
-
-    return (
-      <div className="flex flex-col items-center justify-center">
-        <div ref={waveformRef} />
-        <div className='flex flex-col items-center'>
-          <button onClick={handleSkipBack}>
-            Skip
-          </button>
-          <button onClick={handlePlayPause}>
-            Pause
-          </button>
-          <button onClick={handleStop}>
-            Stop
-          </button>
-          <button onClick={handleSkipForward}>
-            Skip Forward
-          </button>
-        </div>
-        <h1>{title}</h1>
-        <audio controls> 
-            <source src={song}>
-            </source>
-        </audio>
-      </div>
-    ); 
+  const onPlayPause = () => {
+    wavesurfer && wavesurfer.playPause()
   }
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div ref={waveformRef} />
+      <div >
+        <button onClick={onPlayPause}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+      </div>
+      <h1>{title}</h1>
+    </div>
+  ); 
+}
   
