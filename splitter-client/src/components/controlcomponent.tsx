62e6df4957d295aba9ae48b5bc47d12e0f7ff6a4
ethanvar <ethanvar6@gmail.com>
2024-05-27 21:31:24 -0400

    Created <Controlcomponent />

---
+++ splitter-client/src/components/controlcomponent.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React, { useState } from 'react'
+import { Trackdisplay } from '@/components/trackdisplay'
+import { Songsearch } from '@/components/songsearch'
+
+export const Controlcomponent = () =>  {
+    const [title, setTitle] = useState<string>('Track 1')
+    const [currSong, setCurrSong] = useState<string>('https://upload.wikimedia.org/wikipedia/commons/3/33/Ulaw_Lalapa_sakatusa.ogg')
+    
+    const handleSongSelect = (songUrl: any) => {
+        setCurrSong(songUrl);
+    };
+
+    return (
+        <div className='flex min-h-screen items-center justify-center'>
+            <div className='m-10'>
+                <Songsearch setSong={handleSongSelect}/>
+            </div>
+            <div className='m-10'>
+                <Trackdisplay title={title} song={currSong} /> 
+            </div>
+        </div>
+    );
+}
--- splitter-client/src/components/songsearch.tsx
+++ splitter-client/src/components/songsearch.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Trackdisplay } from '@/components/trackdisplay'
 import { useState, FormEvent, ChangeEvent } from 'react';
 
-export const Songsearch = ( { song } : any) => {
+export const Songsearch = ( { setSong } : any) => {
     const [input, setInput] = useState<string>('')
     const [tubeLink, setTubeLink] = useState<string>('')
     const [embedString, setEmbedString] = useState<string>('KMU0tzLwhbE')
@@ -17,31 +17,27 @@ export const Songsearch = ( { song } : any) => {
         setTubeLink(input)
         const lastEight = input.slice(-11)
         setEmbedString(lastEight)
+        setSong(tubeLink)
     }
 
     return (
-        <div className='flex min-h-screen items-center justify-center'> 
-            <div className='flex flex-col items-center rounded-lg shadow-lg p-5 m-10'>
-                <div className='flex flex-row p-4'>
-                    <form onSubmit={handleSubmit} >
-                        <input placeholder='Paste YouTube link' className='border border-green-300 p-4 rounded-lg shadow-lg' onChange={handleChange}></input>
-                        <button type='submit'className='border border-green-300 p-4 rounded-lg shadow-lg m-1'>Go</button>
-                    </form>
-                </div>
-                <div className='p-4'>
-                    <iframe
-                        className='rounded-lg'
-                        width="300"
-                        height="200"
-                        src={`https://www.youtube.com/embed/${embedString}`}
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        title="Embedded youtube"
-                    />
-                </div>
+        <div className='flex flex-col items-center rounded-lg shadow-lg p-5'>
+            <div className='flex flex-row p-4'>
+                <form onSubmit={handleSubmit} >
+                    <input placeholder='Insert YouTube link' className='border border-green-300 p-4 rounded-lg shadow-lg' onChange={handleChange}></input>
+                    <button type='submit'className='border border-green-300 p-4 rounded-lg shadow-lg m-1'>Go</button>
+                </form>
             </div>
-            <div className='m-10'>
-                <Trackdisplay title="Track One" song={song} /> 
+            <div className='p-4'>
+                <iframe
+                    className='rounded-lg'
+                    width="300"
+                    height="200"
+                    src={`https://www.youtube.com/embed/${embedString}`}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                />
             </div>
         </div>
     ); 
