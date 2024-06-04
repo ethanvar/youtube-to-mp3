c4066993e818dc5505a2cf740a59a747fcdd6747
ethanvar <ethanvar6@gmail.com>
2024-06-04 18:53:16 -0400

    Changed <controlcomponent/> to <dashboard/>

--- splitter-client/src/app/page.tsx
+++ splitter-client/src/app/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import axios from 'axios'
-import { Controlcomponent } from '@/components/controlcomponent'
+import { Dashboard } from '@/components/dashboard'
 
 export default async function Page() {
-
-  // const data = await axios('http://127.0.0.1:8800/audio/Reality-Surf.mp3')
-  // console.log(data)
   return (
     <div>
-      <Controlcomponent/>
+      <Dashboard/>
     </div>
   );
 }
--- splitter-client/src/components/dashboard.tsx
+++ splitter-client/src/components/dashboard.tsx
@@ -5,11 +5,14 @@ import { Trackdisplay } from '@/components/trackdisplay'
 import { Songsearch } from '@/components/songsearch'
 import axios from 'axios'
 
-export const Controlcomponent = () =>  {
+export const Dashboard = () =>  {
     const [embedTitle, setEmbedTitle] = useState<string>('')
     const [embedSongUrl, setEmbedSongUrl] = useState<string>('')
-    const [title, setTitle] = useState<string>('Track 1')
-    const [currSong, setCurrSong] = useState<string>('https://upload.wikimedia.org/wikipedia/commons/3/33/Ulaw_Lalapa_sakatusa.ogg')
+
+    const [waveTitle, setWaveTitle] = useState<string>('Track 1')
+    const [waveSong, setWaveSong] = useState<string>('https://upload.wikimedia.org/wikipedia/commons/3/33/Ulaw_Lalapa_sakatusa.ogg')
+
+    const [numberOfTracks, setNumberOfTracks] = useState<number>(1)
 
     
     const handleSongSelect = (songUrl: string) => {
@@ -18,16 +21,16 @@ export const Controlcomponent = () =>  {
     };
     
 
-    const handleSubmit = async (): Promise<void> => {
+    const handleSubmit = async() : Promise<void> => {
         try {
             const resData = {
                 song: embedSongUrl,
                 title: embedTitle
             };
             const res = await axios.post("http://127.0.0.1:8800/api/data", resData).then((response) => {
-                console.log(response.status, response.data.songname);
-                setCurrSong(response.data.url);
-                setTitle(response.data.name)
+                console.log(response.status, response.data);
+                setWaveSong(response.data.url);
+                setWaveTitle(response.data.name)
             });
         } catch (error) {
             console.error('Error sending data', error);
@@ -35,6 +38,10 @@ export const Controlcomponent = () =>  {
         }
     };
     
+    const handleSplit = async() : Promise<void> => {
+        
+    }
+
     return (
         <div className='flex min-h-screen items-center justify-center'>
             <div className='m-10 bg-gray-50 rounded-lg rounded-lg '>
@@ -44,7 +51,7 @@ export const Controlcomponent = () =>  {
                 <button type='submit' onClick={handleSubmit}> Send </button>
             </div>
             <div className='m-10 bg-gray-50 rounded-lg'>
-                <Trackdisplay title={title} song={currSong} /> 
+                <Trackdisplay title={waveTitle} song={waveSong} /> 
             </div>
         </div>
     );
